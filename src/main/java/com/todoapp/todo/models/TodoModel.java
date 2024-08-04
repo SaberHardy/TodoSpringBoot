0d0fe9e95526cc83c82b54d253ef3185411bf93a
SaberHardy <samirsamado5@gmail.com>
2024-08-04 16:46:25 -0700

    create todo model

--- src/main/java/com/todoapp/todo/models/TodoModel.java
+++ src/main/java/com/todoapp/todo/models/TodoModel.java
@@ -1,14 +1,36 @@
 package com.todoapp.todo.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TodoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
-    private String dueDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy")
+    private String createdDate;
     private boolean completed;
+
+    @Override
+    public String toString() {
+        return "TodoModel{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", createdDate='" + createdDate + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
 }
