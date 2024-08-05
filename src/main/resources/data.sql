6b4a73a24f3cc224c301433387544705a550fff5
SaberHardy <samirsamado5@gmail.com>
2024-08-04 18:01:02 -0700

    create dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into todo_model (title, description, due_date, completed)
+values
+ ('test 1', 'desc 1', '01-01-2022',false),
+ ('test 2', 'desc 2', '01-01-2022', false),
+ ('test 3', 'desc 3', '01-01-2022', false);
