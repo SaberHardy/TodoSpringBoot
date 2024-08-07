6db6ea726c34c288a4a7b1ed8b296daef207ab86
SaberHardy <samirsamado5@gmail.com>
2024-08-06 21:34:22 -0700

    EntityScan for main method

--- src/main/java/com/todoapp/todo/TodoApplication.java
+++ src/main/java/com/todoapp/todo/TodoApplication.java
@@ -2,8 +2,10 @@ package com.todoapp.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.todoapp.todo.models")
 public class TodoApplication {
 
 	public static void main(String[] args) {
