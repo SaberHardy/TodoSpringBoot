b1a769d9fd22bab3958244a0dcb66eacc83a5554
SaberHardy <samirsamado5@gmail.com>
2024-08-07 22:03:51 -0700

    create test class for controller

---
+++ src/test/java/com/todoapp/todo/Controllers/TodoControllerTest.java
@@ -0,0 +1,43 @@
+package com.todoapp.todo.Controllers;
+
+import com.todoapp.todo.Services.TodoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TodoControllerTest {
+    @Mock private TodoService todoService;
+
+    @Test
+    void canRetrieveToHomePage() {
+
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void showEditTodo() {
+    }
+
+    @Test
+    void updateTodo() {
+    }
+
+    @Test
+    void deleteTodo() {
+    }
+
+    @Test
+    void deleteTodoByTitle() {
+    }
+}
