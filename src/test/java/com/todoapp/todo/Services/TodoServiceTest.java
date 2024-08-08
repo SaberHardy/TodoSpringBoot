a281d114ad2a3d066d5e623f8e6bb358641772df
SaberHardy <samirsamado5@gmail.com>
2024-08-07 21:03:19 -0700

    use ExtendWith

--- src/test/java/com/todoapp/todo/Services/TodoServiceTest.java
+++ src/test/java/com/todoapp/todo/Services/TodoServiceTest.java
@@ -3,29 +3,32 @@ package com.todoapp.todo.Services;
 import com.todoapp.todo.Repositories.TodoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockitoAnnotations;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
+// @ExtendWith(MockitoExtension.class) is used to create a mock object for the class.
+@ExtendWith(MockitoExtension.class)
 class TodoServiceTest {
 
     @Mock
     private TodoRepository todoRepository;
-    private AutoCloseable autoCloseable;
+    // private AutoCloseable autoCloseable;
     private TodoService todoService;
 
     @BeforeEach
     void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(this);
+        // autoCloseable = MockitoAnnotations.openMocks(this);
         todoService = new TodoService(todoRepository);
     }
 
-    @AfterEach
-    void tearDown() throws Exception {
-        autoCloseable.close();
-    }
+    // @AfterEach
+    // void tearDown() throws Exception {
+    //    autoCloseable.close();
+    // }
 
     @Test
     void getAllTodos() {
