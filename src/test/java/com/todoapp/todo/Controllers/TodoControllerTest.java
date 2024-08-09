07926692735187175e61d51a27cf0b9e8e8dbc42
SaberHardy <samirsamado5@gmail.com>
2024-08-08 21:38:01 -0700

    test canRetrieveToHomePage

--- src/test/java/com/todoapp/todo/Controllers/TodoControllerTest.java
+++ src/test/java/com/todoapp/todo/Controllers/TodoControllerTest.java
@@ -1,20 +1,48 @@
 package com.todoapp.todo.Controllers;
 
+import com.todoapp.todo.Repositories.TodoRepository;
 import com.todoapp.todo.Services.TodoService;
+import com.todoapp.todo.models.TodoModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
+@WebMvcTest(TodoController.class)
 class TodoControllerTest {
-    @Mock private TodoService todoService;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private TodoService todoService;
+
+    //    @BeforeEach
+    //    protected void setUp() {
+    //        mockMvc = MockMvcBuilders.standaloneSetup(new TodoModel()).build();
+    //        // todoController = new TodoController();
+    //    }
 
     @Test
-    void canRetrieveToHomePage() {
+    void canRetrieveToHomePage() throws Exception {
+        String uri = "/";
 
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
     }
 
     @Test
